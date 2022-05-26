3cf38289df5f6c2d069da1a15e0a2fd95c5323cf
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-05-26 12:42:20 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/js.js
@@ -0,0 +1,9 @@
+let js = "amazing but hard to learn";
+function (_call) {
+  if (js === amazing)
+    but; hard; to; learn;
+  return (); do
+    it; while (consistently);
+}
+console.log("call");
+script > ;
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,2 @@
+let name_my = "Keshav" ;
+console.log(name_my); 
