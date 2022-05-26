d1fb59c37c2031c700a7b0681370d3b9ee843f83
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-05-26 20:48:24 +0530

    Delete js.js

--- 01-Fundamentals-Part-1/starter/js.js
+++
@@ -1,9 +0,0 @@
-let js = "amazing but hard to learn";
-function (_call) {
-  if (js === amazing)
-    but; hard; to; learn;
-  return (); do
-    it; while (consistently);
-}
-console.log("call");
-script > ;
