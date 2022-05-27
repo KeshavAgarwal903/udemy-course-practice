eac866c74f54fd19d4093bfc2fe9de9f993c0822
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-05-27 12:36:05 +0530

    Add files via upload

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,30 +1,42 @@
 //VALUE AND VARIABLES
 
-let country = 'India , America , Canada';
+// let country = 'India , America , Canada';
 
-let continent = 'Asia , USA , Europe';
+// let continent = 'Asia , USA , Europe';
 
-let polulation = '130 million , NIL , NIL';
+// let polulation = '130 million , NIL , NIL';
 
-console.log(country , continent , polulation); 
+// console.log(country , continent , polulation); 
  
-//The Second Method 
+// //The Second Method 
 
-let countryDetails = {
-  country: 'India , America , Canada',
+// let countryDetails = {
+//   country: 'India , America , Canada',
 
-  continent: 'Asia , USA , Europe',
+//   continent: 'Asia , USA , Europe',
 
-  population: '130 million , NIL , NIL',
-};
+//   population: '130 million , NIL , NIL',
+// };
 
-console.log(countryDetails); 
+// console.log(countryDetails); 
 
 // DATA TYPES
 
-let nation = 'India'; // use according to your nation
-let isIsland = false;
-let popolation = 130 ;
-let language;
+// let nation = 'India'; // use according to your nation
+// let isIsland = false;
+// let population = 130 ;
 
-console.log(typeof nation , typeof isIsland , typeof population , typeof language);
+// console.log(typeof nation , typeof isIsland , typeof population , typeof language);
+
+
+//LET , CONST AND VAR
+let language = "Hindi";
+
+//The value of the nation and isIsland will be const and never change
+
+const  country = 'India'; 
+const isIsland = false;
+const population = 130 ;
+population = 140;
+
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,19 @@
+// VALUES AND VARIABLES
+
 let name_my = "Keshav" ;
 console.log(name_my); 
 
+// DATA TYPES
 
 let javascriptIsFun = true ;
 
 javascriptIsFun = "YES!" 
 
 console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+//LET , CONST AND VAR
+
+const birthYear = 2006;
+console.log(typeof birthYear)
+// birthYear = 2005;
