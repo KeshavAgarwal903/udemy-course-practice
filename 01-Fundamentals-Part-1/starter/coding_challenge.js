7ec4a0204228233ab395c86addec7d2bb69ffa89
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-05-29 12:17:40 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/coding_challenge.js
@@ -0,0 +1,17 @@
+// Coding Challenge Solution #1
+
+let massMark = 78;
+let heigthMark = 1.69;
+
+let markBMI = massMark / heigthMark ** 2;
+console.log(markBMI);
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let johnBMI = massJohn / heightJohn ** 2;
+console.log(johnBMI);
+
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+
