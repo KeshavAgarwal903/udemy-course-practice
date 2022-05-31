2a54626290b7345519c6ff056a349b5d9bd94ec0
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-05-31 12:37:10 +0530

    Add files via upload

--- 01-Fundamentals-Part-1/starter/coding_challenge.js
+++ 01-Fundamentals-Part-1/starter/coding_challenge.js
@@ -1,17 +1,24 @@
-// Coding Challenge Solution #1
+// 🚀Coding Challenge Solution #1
 
 let massMark = 78;
 let heigthMark = 1.69;
 
 let markBMI = massMark / heigthMark ** 2;
-console.log(markBMI);
+// console.log(markBMI);
 
 let massJohn = 92;
 let heightJohn = 1.95;
 
 let johnBMI = massJohn / heightJohn ** 2;
-console.log(johnBMI);
+// console.log(johnBMI);
 
-let markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+// let markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
 
+//🚀 Coding Challenge Solution #2
+
+if(markBMI > johnBMI){
+  console.log(`Marks BMi (${markBMI}) is higher than Johns (${johnBMI})!`)
+} else {
+  console.log(`Johns BMI (${johnBMI}) is higher than Marks(${markBMI})!`)
+}
