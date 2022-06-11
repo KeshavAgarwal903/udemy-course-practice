50102ef46a57872ec18ab8be1f9d38157a7e0413
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-06-11 11:25:45 +0530

    Add files via upload

--- Fundamentals-Part-2/script.js
+++ Fundamentals-Part-2/script.js
@@ -42,15 +42,15 @@
 // console.log(appleOrangeJuice);
 
 //FUNCTION DECLARATION
-function calcAge1(birthYear){
-  return 2037 - birthYear;
-}
-const age1 = calcAge1(1991)
+// function calcAge1(birthYear){
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991)
 
-//FUNCTION EXPRESSION
-const calcAge2 = function (birthYear){
-  return 2037 - birthYear;
-}
-const age2 = calcAge2(1991);
+// //FUNCTION EXPRESSION
+// const calcAge2 = function (birthYear){
+//   return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
 
-console.log(age1 , age2);
+// console.log(age1 , age2);
