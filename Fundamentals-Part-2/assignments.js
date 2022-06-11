327bb11bb8ac48e3059f42344e3b0836917f6cfb
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-06-11 11:26:21 +0530

    Add files via upload

--- Fundamentals-Part-2/assignments.js
+++ Fundamentals-Part-2/assignments.js
@@ -1,10 +1,28 @@
 'use stirct';
 // FUNCTIONS
 
-function describeCountry(country , population , capitalCity){
+// function describeCountry(country , population , capitalCity){
 
-  return `${country} has ${population} million people and it capital city is ${capitalCity}`;
+//   return `${country} has ${population} million people and it capital city is ${capitalCity}`;
+// }
+
+// const finland = describeCountry(`finland` , 6 , `Helsinki`);
+// console.log(finland);
+
+// FUNCTION DECLARATION VS EXPRESSION
+
+function percentageOfWorld1(population){
+  return (population/7900)*100
+};
+
+console.log(percentageOfWorld1(1300)); 
+console.log(percentageOfWorld1(1400)); 
+console.log(percentageOfWorld1(1000));
+
+const percentageOfWorld2 = function(population1){
+  return (population1/7900)*100
 }
 
-const finland = describeCountry(`finland` , 6 , `Helsinki`);
-console.log(finland);
+console.log(percentageOfWorld2(1300));
+console.log(percentageOfWorld2(1400));
+console.log(percentageOfWorld2(1000));
