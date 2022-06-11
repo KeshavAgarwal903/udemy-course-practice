0adf325cc42a1d539b7ceea93cb7aa5b9b7e3617
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-06-11 11:52:53 +0530

    Add files via upload

--- Fundamentals-Part-2/assignments.js
+++ Fundamentals-Part-2/assignments.js
@@ -11,18 +11,26 @@
 
 // FUNCTION DECLARATION VS EXPRESSION
 
-function percentageOfWorld1(population){
-  return (population/7900)*100
-};
+// function percentageOfWorld1(population){
+//   return (population/7900)*100
+// };
 
-console.log(percentageOfWorld1(1300)); 
-console.log(percentageOfWorld1(1400)); 
-console.log(percentageOfWorld1(1000));
+// console.log(percentageOfWorld1(1300)); 
+// console.log(percentageOfWorld1(1400)); 
+// console.log(percentageOfWorld1(1000));
 
-const percentageOfWorld2 = function(population1){
-  return (population1/7900)*100
-}
+// const percentageOfWorld2 = function(population1){
+//   return (population1/7900)*100
+// }
 
-console.log(percentageOfWorld2(1300));
-console.log(percentageOfWorld2(1400));
-console.log(percentageOfWorld2(1000));
+// console.log(percentageOfWorld2(1300));
+// console.log(percentageOfWorld2(1400));
+// console.log(percentageOfWorld2(1000));
+
+// // ARROW FUNCTIONS
+
+// const percentageOfWorld3 = population3 => (population2/7900)*100;
+
+// console.log(percentageOfWorld3(1300));
+// console.log(percentageOfWorld3(1400));
+// console.log(percentageOfWorld3(1000));
