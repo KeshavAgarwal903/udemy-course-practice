2469d9f35594955849a85cbcde435fdf23572938
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-06-16 12:02:37 +0530

    Add files via upload

--- Fundamentals-Part-2/coding_challenges.js
+++ Fundamentals-Part-2/coding_challenges.js
@@ -2,17 +2,17 @@
 
 //CODING CHALLENGE 1
 
-const averageDolphins = (85+54+41)/3;
-const averageKoalas = (23+34+27)/3;
+// const averageDolphins = (85+54+41)/3;
+// const averageKoalas = (23+34+27)/3;
 
-function checkWinner(avgDolphins , avgKoalas){
-  if(avgDolphins >= 2*avgKoalas){
-    console.log(`Dolphin wins (${averageDolphins} vs ${averageKoalas})`)
-  }else if(avgKoalas >= 2*avgDolphins){
-    console.log(`Koalas wins (${averageKoalas} vs ${averageDolphins})`)
-  }else{
-    console.log(`No team wins the match :(`)
-  }
-}
+// function checkWinner(avgDolphins , avgKoalas){
+//   if(avgDolphins >= 2*avgKoalas){
+//     console.log(`Dolphin wins (${averageDolphins} vs ${averageKoalas})`)
+//   }else if(avgKoalas >= 2*avgDolphins){
+//     console.log(`Koalas wins (${averageKoalas} vs ${averageDolphins})`)
+//   }else{
+//     console.log(`No team wins the match :(`)
+//   }
+// }
 
-console.log(checkWinner(averageDolphins , averageKoalas));
+// console.log(checkWinner(averageDolphins , averageKoalas));
