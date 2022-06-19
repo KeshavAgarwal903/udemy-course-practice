13c4c2e8aaca9506414477ce251cbdaa7a138a77
Keshav Agarwal <92775450+KeshavAgarwal903@users.noreply.github.com>
2022-06-19 18:13:50 +0530

    Add files via upload

--- Fundamentals-Part-2/assignments.js
+++ Fundamentals-Part-2/assignments.js
@@ -59,3 +59,27 @@
 // const percentageOfWorld2 = function(population1){
 //   return (population1/7900)*100
 // }
+
+// Basic Array Operations (Methods)
+
+const neighbours = [`China` , `Pakistan` , `Sri lanka`, `Sweden`];
+
+neighbours.push(`Utopia`);
+console.log(neighbours)
+
+neighbours.pop();
+console.log(neighbours);
+
+if(neighbours.includes(`Germany`)){
+  console.log(`Its a central European country :/`)
+}else{
+  console.log(`Probably not a central European country :D`)
+}
+
+const change = neighbours.indexOf(`Sweden`);
+
+if (change !== 0){
+  neighbours[change] = `Republic of Sweden`;
+};
+
+console.log(neighbours);
